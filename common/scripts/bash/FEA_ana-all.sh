055db8eebd9463242d447168cb068c0c67b4002c
wolff <wolff@05f62fea-98e3-1e44-aab5-ce008673666c>
2016-06-08 16:46:06 +0000

    o fixed file lists of Versuch004/B,C o added bash scripts
    git-svn-id: https://jamaika.kt.tu-cottbus.de:8443/svn/databases/uasr-data-bottles/trunk@100 05f62fea-98e3-1e44-aab5-ce008673666c

---
+++ common/scripts/bash/FEA_ana-all.sh
@@ -0,0 +1,14 @@
+cd $UASR_HOME-data/bottles
+
+cd    Versuch004_A;    nohup HMM.xtp trn info/Versuch004_A.mcfg    2>&1 &
+cd ../Versuch004_B;    nohup HMM.xtp trn info/Versuch004_B.mcfg    2>&1 &
+cd ../Versuch004_C;    nohup HMM.xtp trn info/Versuch004_C.mcfg    2>&1 &
+cd ../Vorversuch001_A; nohup HMM.xtp trn info/Vorversuch001_A.mcfg 2>&1 &
+cd ../Vorversuch001_B; nohup HMM.xtp trn info/Vorversuch001_B.mcfg 2>&1 &
+cd ../Vorversuch001_C; nohup HMM.xtp trn info/Vorversuch001_C.mcfg 2>&1 &
+cd ../Vorversuch002_A; nohup HMM.xtp trn info/Vorversuch002_A.mcfg 2>&1 &
+cd ../Vorversuch002_B; nohup HMM.xtp trn info/Vorversuch002_B.mcfg 2>&1 &
+cd ../Vorversuch002_C; nohup HMM.xtp trn info/Vorversuch002_C.mcfg 2>&1 &
+cd ../Vorversuch003_A; nohup HMM.xtp trn info/Vorversuch003_A.mcfg 2>&1 &
+cd ../Vorversuch003_B; nohup HMM.xtp trn info/Vorversuch003_B.mcfg 2>&1 &
+cd ../Vorversuch003_C; nohup HMM.xtp trn info/Vorversuch003_C.mcfg 2>&1 &
---
+++ common/scripts/bash/HMM_trn-all.sh
@@ -0,0 +1,14 @@
+cd $UASR_HOME-data/bottles
+
+cd    Versuch004_A;    nohup FEA.xtp ana info/Versuch004_A.mcfg    2>&1 &
+cd ../Versuch004_B;    nohup FEA.xtp ana info/Versuch004_B.mcfg    2>&1 &
+cd ../Versuch004_C;    nohup FEA.xtp ana info/Versuch004_C.mcfg    2>&1 &
+cd ../Vorversuch001_A; nohup FEA.xtp ana info/Vorversuch001_A.mcfg 2>&1 &
+cd ../Vorversuch001_B; nohup FEA.xtp ana info/Vorversuch001_B.mcfg 2>&1 &
+cd ../Vorversuch001_C; nohup FEA.xtp ana info/Vorversuch001_C.mcfg 2>&1 &
+cd ../Vorversuch002_A; nohup FEA.xtp ana info/Vorversuch002_A.mcfg 2>&1 &
+cd ../Vorversuch002_B; nohup FEA.xtp ana info/Vorversuch002_B.mcfg 2>&1 &
+cd ../Vorversuch002_C; nohup FEA.xtp ana info/Vorversuch002_C.mcfg 2>&1 &
+cd ../Vorversuch003_A; nohup FEA.xtp ana info/Vorversuch003_A.mcfg 2>&1 &
+cd ../Vorversuch003_B; nohup FEA.xtp ana info/Vorversuch003_B.mcfg 2>&1 &
+cd ../Vorversuch003_C; nohup FEA.xtp ana info/Vorversuch003_C.mcfg 2>&1 &
