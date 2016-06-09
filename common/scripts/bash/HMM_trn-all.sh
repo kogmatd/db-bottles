ea3d684276fd10f86c2f155b6a0888ed49f8e551
wolff <wolff@05f62fea-98e3-1e44-aab5-ce008673666c>
2016-06-09 12:23:38 +0000

    o [fixed] batch scripts o ignored training results
    git-svn-id: https://jamaika.kt.tu-cottbus.de:8443/svn/databases/uasr-data-bottles/trunk@101 05f62fea-98e3-1e44-aab5-ce008673666c

--- common/scripts/bash/HMM_trn-all.sh
+++ common/scripts/bash/HMM_trn-all.sh
@@ -1,14 +1,16 @@
-cd $UASR_HOME-data/bottles
+#!/bin/bash
 
-cd    Versuch004_A;    nohup FEA.xtp ana info/Versuch004_A.mcfg    2>&1 &
-cd ../Versuch004_B;    nohup FEA.xtp ana info/Versuch004_B.mcfg    2>&1 &
-cd ../Versuch004_C;    nohup FEA.xtp ana info/Versuch004_C.mcfg    2>&1 &
-cd ../Vorversuch001_A; nohup FEA.xtp ana info/Vorversuch001_A.mcfg 2>&1 &
-cd ../Vorversuch001_B; nohup FEA.xtp ana info/Vorversuch001_B.mcfg 2>&1 &
-cd ../Vorversuch001_C; nohup FEA.xtp ana info/Vorversuch001_C.mcfg 2>&1 &
-cd ../Vorversuch002_A; nohup FEA.xtp ana info/Vorversuch002_A.mcfg 2>&1 &
-cd ../Vorversuch002_B; nohup FEA.xtp ana info/Vorversuch002_B.mcfg 2>&1 &
-cd ../Vorversuch002_C; nohup FEA.xtp ana info/Vorversuch002_C.mcfg 2>&1 &
-cd ../Vorversuch003_A; nohup FEA.xtp ana info/Vorversuch003_A.mcfg 2>&1 &
-cd ../Vorversuch003_B; nohup FEA.xtp ana info/Vorversuch003_B.mcfg 2>&1 &
-cd ../Vorversuch003_C; nohup FEA.xtp ana info/Vorversuch003_C.mcfg 2>&1 &
+DBHOME=$UASR_HOME-data/bottles
+
+cd $DBHOME/Versuch004_A;    nohup HMM.xtp trn info/Versuch004_A.mcfg    2>&1 &
+cd $DBHOME/Versuch004_B;    nohup HMM.xtp trn info/Versuch004_B.mcfg    2>&1 &
+cd $DBHOME/Versuch004_C;    nohup HMM.xtp trn info/Versuch004_C.mcfg    2>&1 &
+#cd $DBHOME/Vorversuch001_A; nohup HMM.xtp trn info/Vorversuch001_A.mcfg 2>&1 &
+#cd $DBHOME/Vorversuch001_B; nohup HMM.xtp trn info/Vorversuch001_B.mcfg 2>&1 &
+#cd $DBHOME/Vorversuch001_C; nohup HMM.xtp trn info/Vorversuch001_C.mcfg 2>&1 &
+#cd $DBHOME/Vorversuch002_A; nohup HMM.xtp trn info/Vorversuch002_A.mcfg 2>&1 &
+#cd $DBHOME/Vorversuch002_B; nohup HMM.xtp trn info/Vorversuch002_B.mcfg 2>&1 &
+#cd $DBHOME/Vorversuch002_C; nohup HMM.xtp trn info/Vorversuch002_C.mcfg 2>&1 &
+#cd $DBHOME/Vorversuch003_A; nohup HMM.xtp trn info/Vorversuch003_A.mcfg 2>&1 &
+#cd $DBHOME/Vorversuch003_B; nohup HMM.xtp trn info/Vorversuch003_B.mcfg 2>&1 &
+#cd $DBHOME/Vorversuch003_C; nohup HMM.xtp trn info/Vorversuch003_C.mcfg 2>&1 &
