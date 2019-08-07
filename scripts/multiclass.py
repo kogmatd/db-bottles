35204e3c4604f69dae1b79722154e113bfa961ec
Kraljevski <ivan.kraljevski@ikts.fraunhofer.de>
2019-08-07 16:33:52 +0200

    Experimental scripts

--- scripts/multiclass.py
+++ scripts/multiclass.py
@@ -1,119 +1,205 @@
 #!/usr/bin/python3
 
-import os
 import sys
-import time
-
+import os
 sys.path.append(os.environ['UASR_HOME']+'-py')
-
-import isvm
-#import imodsvm
-import ihmm
-import idnn
-import iktf
-import ijob
 import ifdb
+import iktf
+import ihmm
+import isvm
 from ihelp import *
-icnn=idnn
 
-def svmtrn(ftrn,ftst,fea,s,kwargs={}):
+
+def svmtrn(ftrn, ftst, fea, s, args):
+
     print('svm start  '+fea+'_'+s)
-    csvm=isvm.trn(ftrn, fea, **kwargs)
-    restrn=isvm.evl(csvm, ftrn, fea)
-    restst=isvm.evl(csvm, ftst, fea)
-    print('svm finish '+fea+'_'+s)
-    return (csvm,restrn,restst)
-
-def hmmtrn(ftrn,ftst,fea,s,kwargs={}):
-    print('hmm start  '+fea+'_'+s)
-    chmm=ihmm.trn(flst=ftrn,fea=fea,its=[0],states=3,**kwargs)
-    nldtrn=ihmm.evlp(chmm,flst=ftrn,fea=fea)
-    nldtst=ihmm.evlp(chmm,flst=ftst,fea=fea)
-    if icfg.get('exp')=='triclass' or icfg.get('trn.regression')==True:
-        print('hmm finish '+fea+'_'+s)
+    csvm = isvm.trn(ftrn, fea, **args)
+    restrn = isvm.evlp(csvm, ftrn, fea)
+    restst = isvm.evlp(csvm, ftst, fea)
+    if regression:
+        print('hmm finish ' + fea + '_' + s)
+        icls.labf(ftrn)
+        icls.labf(ftst)
+        trn = list(map(lambda x: x['labf'], ftrn))
+        tst = list(map(lambda x: x['labf'], ftst))
+        print('Train mse : ', np.square(np.subtract(np.array(trn), restrn)).mean())
+        print('Test mse : ', np.square(np.subtract(np.array(tst), restst)).mean())
     else:
-        res=np.array(chmm['cls'])[nldtst.argmin(axis=1)]
-        icls.cmp(ftst,res,'hmm finish '+fea+'_'+s)
-    return (chmm,nldtrn,nldtst)
+        restrnc = isvm.evl(csvm, ftrn, fea)
+        reststc = isvm.evl(csvm, ftst, fea)
+        icls.cmp(ftrn, restrnc, 'SVM Training ' + fea + '_' + s)
+        icls.cmp(ftst, reststc, 'SVM Testing ' + fea + '_' + s)
+    print('svm finish '+fea+'_'+s)
+    return csvm, restrn, restst
 
-def ktftrn(ftrn,ftst,fea,s,kwargs={}):
-    print('dnn start  '+s)
-    ktf = iktf.ModKeras()
+
+def hmmtrn(ftrn, ftst, fea, s, args):
+    print('hmm start  '+fea+'_'+s)
+    chmm = ihmm.trn(flst=ftrn, fea=fea, **args, ftst=ftst)
+    nldtrn = ihmm.evlp(chmm, flst=ftrn, fea=fea)
+    nldtst = ihmm.evlp(chmm, flst=ftst, fea=fea)
+    if regression:
+        icls.labf(ftrn)
+        icls.labf(ftst)
+        trn = list(map(lambda x: x['labf'], ftrn))
+        tst = list(map(lambda x: x['labf'], ftst))
+        print('Train mse : ', np.square(np.subtract(np.array(trn), nldtrn)).mean())
+        print('Test mse : ', np.square(np.subtract(np.array(tst), nldtst)).mean())
+    else:
+        restrn = np.array(chmm['cls'])[nldtrn.argmin(axis=1)]
+        icls.cmp(ftrn, restrn, 'HMM Training '+fea+'_'+s)
+        restst = np.array(chmm['cls'])[nldtst.argmin(axis=1)]
+        icls.cmp(ftst, restst, 'HMM Testing ' + fea + '_' + s)
+    return chmm, nldtrn, nldtst
+
+
+def ktftrn(ftrn, ftst, fea, s, args):
+    print('Keras TF start  ' + s)
+    ktf = iktf.ModKeras(**args)
     ktf.trn(ftrn, fea)
-    restrn=ktf.evl(ftrn, fea, prob=True)
-    restst=ktf.evl(ftst, fea, prob=True)
-    print('dnn stop  ' + s)
-    return (ktf.mod,restrn,restst)
+    restrn, trncls = ktf.evl(ftrn, fea, prob=True)
+    restst, tstcls = ktf.evl(ftst, fea, prob=True)
+    if regression:
+        icls.labf(ftrn)
+        icls.labf(ftst)
+        trn = list(map(lambda x: x['labf'], ftrn))
+        tst = list(map(lambda x: x['labf'], ftst))
+        print('Train mse : ', np.square(np.subtract(np.array(trn), restrn[:, 0])).mean())
+        print('Test mse : ', np.square(np.subtract(np.array(tst), restst[:, 0])).mean())
+    else:
+        icls.cmp(ftrn, trncls, 'Keras TF Training ' + fea + '_' + s)
+        icls.report(ftrn, trncls, verbose=True)
+        icls.cmp(ftst, tstcls, 'Keras TF Testing ' + fea + '_' + s)
+        icls.report(ftst, tstcls, verbose=True)
+    print('Keras TF stop  ' + s)
+    return ktf.mod, restrn, restst
 
-if len(sys.argv)<2: raise ValueError("Usage: "+sys.argv[0]+" CFG [-n]")
+
+snntrn = ktftrn
+aectrn = ktftrn
+rnntrn = ktftrn
+cnntrn = ktftrn
+
+isnn = iktf
+iaec = iktf
+irnn = iktf
+icnn = iktf
+
+if len(sys.argv) < 2:
+    raise ValueError("Usage: "+sys.argv[0]+" CFG [-n]")
 icfg.Cfg(*sys.argv[1:])
 
-ftrn=icfg.readflst('train')
-ftst=icfg.readflst('test')
+ftrain = icfg.readflst('train')
+ftest = icfg.readflst('test')
 
-dmod=icfg.getdir('model')
-dlog=icfg.getdir('log')
+dmod = icfg.getdir('model')
+if not os.path.exists(dmod):
+    os.mkdir(dmod)
+dlog = icfg.getdir('log')
+if not os.path.exists(dlog):
+    os.mkdir(dlog)
 
-feause=['pfa','sfa','sig']
-clsuse=['hmm','svm','ktf']
-if not icfg.get('feause') is None: feause=icfg.get('feause').split(',')
-if not icfg.get('clsuse') is None: clsuse=icfg.get('clsuse').split(',')
+sen = getsensors()
+regression = icfg.get('trn.regression') == True
 
-regression=False
-maxjob=18
-s = 'bottles'
+senuse = sen
+feause = ['pfa', 'sfa', 'sig']
+clsuse = ['hmm', 'svm', 'snn', 'cnn']
+if not icfg.get('senuse') is None:
+    senuse = icfg.get('senuse').split(',')
+if not icfg.get('feause') is None:
+    feause = icfg.get('feause').split(',')
+if not icfg.get('clsuse') is None:
+    clsuse = icfg.get('clsuse').split(',')
 
-if '-nn' in sys.argv: raise SystemExit()
+labmap = getlabmaps()
+if not icfg.get('labmap') is None:
+    labmap = icfg.get('labmap')
+    labmap = eval(labmap)
 
-# run sessions
+maxjob = 16
 
-print("flst [%s]"%(s))
-fdb=ifdb.Fdb()
-for typ in ['sig', 'pfa']:
-    fdb.analyse(typ, eval(typ+'get'), ftrn+ftst)
+if '-nn' in sys.argv:
+    raise SystemExit()
 
-sfaget(ftrn, ftst, fdb)
-fdb.save()
 
-'''Not for highly unbalanced datasets'''
-#ftrn=ftrn.equalcls()
+def run_sen(s):
+    print("flst [%s]" % s) if s is not None else print("flst [%s]" % icfg.get('db'))
+    fdb = ifdb.Fdb(s)
+    ftrns = ftrain
+    ftsts = ftest
+    if s is not None:
+        ftrns = ftrns.expandsensor(s, fdb)
+        ftsts = ftsts.expandsensor(s, fdb)
+    if labmap is not None:
+        ftrns = ftrns.maplab(labmap)
+        ftsts = ftsts.maplab(labmap)
 
-for cls in clsuse:
-    for fea in feause:
-        if cls=='hmm' and ftrn[0][fea].shape[-1]>40 :
-            continue
-        resfn=os.path.join(dlog,'res_'+cls+'_'+fea+'_'+s+'.npy')
-        if os.path.exists(resfn):
-            continue
-        print('####################',fea,cls,s,'####################')
+    trn_labels = list(map(lambda x: x['lab'], ftrns))
+    trn_nclasses = len(set(trn_labels))
 
-        kwargs=icfg.get('trnargs.%s.%s'%(cls,fea))
+    if trn_nclasses < 2 and not regression:
+        raise ValueError("Multiclass classification cannot be trained on one class")
 
-        if kwargs is None:
-            if cls=='dnn' or cls[:3]=='cnn': continue
-            kwargs={}
-        else:
-            print('trnargs = '+kwargs)
-            kwargs=eval(kwargs)
-        kwargs['regression']=regression
+    for typ in ['sig', 'pfa']:
+        fdb.analyse(typ, eval(typ + 'get'), ftrns + ftsts)
+    sfaget(ftrns, ftsts, fdb)
+    fdb.save()
 
-        fnctrn=eval(cls[:3]+'trn')
+    if s is None:
+        s = icfg.get('db')
+    if not regression:
+        ftrns = ftrns.equalcls()
+    if '-n' in sys.argv:
+        return
 
-        for i in range(3):
-            (mod, restrn, restst) = fnctrn(ftrn, ftst, fea, s, kwargs)
-            if len(restst)>0:
-                np.save(resfn,restst)
-                np.save(resfn[:-4]+'_trn.npy',restrn)
-                eval('i'+cls[:3]+'.save')(mod,resfn[:-4]+'.model')
+    for cls in clsuse:
+        for fea in feause:
+            if cls == 'hmm' and ftrns[0][fea].shape[-1] > 40:
+                continue
+            resfn = os.path.join(dlog, 'res_'+cls+'_'+fea+'_'+s+'.npy')
+
+            print('####################', fea, cls, s, '####################')
+            kwargs = icfg.get('trnargs.%s.%s' % (cls, fea))
+            if kwargs is None:
+                kwargs = dict()
+                if cls == 'hmm':
+                    split, its = getsplits()
+                    classes, states = getstates(labmap)
+                    if its is not None:
+                        kwargs['its'] = its
+                    if states is not None:
+                        kwargs['states'] = states
+            else:
+                print('trnargs = '+kwargs)
+                kwargs = eval(kwargs)
+            kwargs['regression'] = regression
+            if cls not in ['hmm', 'svm']:
+                kwargs['type'] = cls
+            fnctrn = eval(cls[:3]+'trn')
+            for i in range(3):
+                (mod, restrn, restst) = fnctrn(ftrns, ftsts, fea, s, kwargs)
+
+                if len(restst) > 0:
+                    np.save(resfn[:-4] + '_tst.npy', restst)
+                if len(restrn) > 0:
+                    np.save(resfn[:-4] + '_trn.npy', restrn)
+                if mod is not None:
+                    modfn = os.path.join(dmod, cls + '_' + fea + '_' + s + '.model')
+                    eval('i' + cls[:3] + '.save')(mod, modfn)
                 break
 
 
-
-#job=ijob.Job(maxjob)
-
-#if os.path.exists('stop'): job.cleanup(); raise SystemExit()
-#job.start('run_'+s,run,(s,))
-#job.res('run_'+s)
+if senuse is None:
+    run_sen(senuse)
+elif len(senuse) == 1:
+    run_sen(senuse[0])
+else:
+    # job=ijob.Job(1 if len([cls for cls in clsuse if cls=='dnn' or cls[:3]=='cnn'])>0 else maxjob)
+    for sns in senuse:
+        run_sen(sns)
+    #    if os.path.exists('stop'): job.cleanup(); raise SystemExit()
+    #    job.start('run_'+s,run_sen,(s,))
+    # for s in senuse: job.res('run_'+s)
 
 raise SystemExit()
-
